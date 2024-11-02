064620ac603d1a5fdeaec0b34f90947dc68fa2ea
Dmitrii <ladovod@gmail.com>
2024-11-03 03:10:14 +0500

    Add function to update data in balances table, add some styles

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,7 +4,6 @@
  * @returns {Promise<any>}
  */
 const getUserData = async (userId) => {
-  //console.log(data.access_token)
   const api_url = '/api.php?userId=' + userId ;
   const response = await fetch(api_url, {
     method: 'GET',
@@ -17,21 +16,59 @@ const getUserData = async (userId) => {
   return await response.json();
 };
 
+/**
+ * ОБновление таблицы
+ * @param data
+ */
+function UpdateBalancesData(data) {
+  // Clear data
+  document.querySelector("#balances > tbody").innerHTML = "";
+
+  // Get a reference to the table body
+  const bodySection = document.querySelectorAll("tbody")[0];
+  //var tableRef = bodySection
+
+
+  // В цикле добавлять данные из json
+  for (let i = 0; i < data.length; i++) {
+    var newRow = bodySection.insertRow(i);
+
+    // Insert a cell in the row at index 0
+    var accountCell = newRow.insertCell(0);
+    var monthCell = newRow.insertCell(1);
+    var balanceCell = newRow.insertCell(2);
+
+    // Append a text node to the cell
+    var accountText = document.createTextNode(data[i].account);
+    var MonthText = document.createTextNode(data[i].month);
+    var BalanceText = document.createTextNode(data[i].balance);
+    accountCell.appendChild(accountText);
+    monthCell.appendChild(MonthText);
+    balanceCell.appendChild(BalanceText);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function (e) {
   console.log('index el');
   document.querySelector('#users').onchange = function (event) {
     console.log(event.target.value);
-    const userId = event.target.value;
-    getUserData(userId)
-      .then(
-        data => {
-          console.log(data);
-          //setProduct(data)
-        })
-      .catch((error) => {
-        // @todo обработать ошибку
-        console.log(error);
-      });
+    if (event.target.value !== '_none') {
+      const userId = event.target.value;
+      getUserData(userId)
+        .then(
+          data => {
+            console.log(data);
+            UpdateBalancesData(data);
+
+          })
+        .catch((error) => {
+          // @todo обработать ошибку
+          console.log(error);
+        });
+    }
+    else {
+      document.querySelector("#balances > tbody").innerHTML = "";
+    }
 
   };
 
