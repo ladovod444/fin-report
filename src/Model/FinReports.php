71c7017e44d82f6b370726a1fb2be9bf68247c4f
Dmitrii <ladovod@gmail.com>
2024-11-04 01:08:52 +0500

    Fix user balance query one more

--- src/Model/FinReports.php
+++ src/Model/FinReports.php
@@ -6,7 +6,9 @@ use Exception;
 
 class FinReports extends \PDO
 {
+
     public const DSN = "sqlite:file:../db/db.sqlite";
+
     function __construct()
     {
         parent::__construct(self::DSN);
@@ -14,42 +16,41 @@ class FinReports extends \PDO
 
     function getUserData()
     {
-
-
         //var_dump($users); die();
         //$users = $this->query("SELECT * FROM users ORDER BY id DESC")->fetchAll();
         // @TODO "В выпадающем списке должны отображаться только те пользователи у которых имеются транзакции"
 
         // @todo добавить проверку
 
-        $users = $this->query("SELECT * FROM users  WHERE id IN (
+        $users = $this->query(
+          "SELECT * FROM users  WHERE id IN (
         SELECT user_id from user_accounts ua WHERE 
         id in (SELECT DISTINCT account_to FROM transactions)
            OR 
         id in (SELECT DISTINCT account_from FROM transactions)
-        )")->fetchAll();
+        )"
+        )->fetchAll();
 
         //var_dump($users);
 
         return $users;
-
     }
 
     public function getUserMonthBalance($userId): false|array
     {
-//        $sql_income = "WITH froms AS (
-//    SELECT sum(amount) AS summary_from, t.account_from as from_account, t.account_to, strftime('%m', t.trdate) as month_from
-//FROM transactions t
-//WHERE t.account_from IN (SELECT id from user_accounts ua WHERE ua.user_id=:user_id)
-//GROUP BY from_account, month_from
-//)
-//SELECT sum(amount) AS SUM_TO, COALESCE(summary_from, 0), (sum(amount) - COALESCE(summary_from, 0)) AS balance, t.account_to TO_ACC, strftime('%m', t.trdate) as Month_TO
-//FROM transactions t
-//FULL JOIN froms AS froms ON MONTH_TO = month_from AND from_account = t.account_to
-//WHERE t.account_to IN (SELECT id from user_accounts ua WHERE ua.user_id=:user_id)
-//GROUP BY TO_ACC, MONTH_TO";
+        //        $sql_income = "WITH froms AS (
+        //    SELECT sum(amount) AS summary_from, t.account_from as from_account, t.account_to, strftime('%m', t.trdate) as month_from
+        //FROM transactions t
+        //WHERE t.account_from IN (SELECT id from user_accounts ua WHERE ua.user_id=:user_id)
+        //GROUP BY from_account, month_from
+        //)
+        //SELECT sum(amount) AS SUM_TO, COALESCE(summary_from, 0), (sum(amount) - COALESCE(summary_from, 0)) AS balance, t.account_to TO_ACC, strftime('%m', t.trdate) as Month_TO
+        //FROM transactions t
+        //FULL JOIN froms AS froms ON MONTH_TO = month_from AND from_account = t.account_to
+        //WHERE t.account_to IN (SELECT id from user_accounts ua WHERE ua.user_id=:user_id)
+        //GROUP BY TO_ACC, MONTH_TO";
 
-        $sql_income="
+        $sql_income = "
         WITH selected_user_accounts AS (SELECT id from user_accounts ua WHERE ua.user_id=:user_id),
         
         from_transactions AS (
@@ -65,10 +66,10 @@ class FinReports extends \PDO
     GROUP BY to_account, month_to)
 
     
-SELECT COALESCE(from_account, tt.to_account) as account, COALESCE(summary_from, 0) as SUM_FROM, month_from,
+SELECT COALESCE(from_account, tt.to_account) as account, COALESCE(summary_from, 0), month_from,
 
 
-COALESCE(tt.summary_to, 0) as SUM_TO, tt.to_account, COALESCE(tt.month_to, month_from) as month,
+COALESCE(tt.summary_to, 0), tt.to_account, COALESCE(tt.month_to, month_from) as month,
 
 COALESCE(tt.summary_to, 0) - COALESCE(summary_from, 0) AS balance
 
@@ -82,7 +83,7 @@ ORDER BY account, month
         $sth->execute([":user_id" => $userId]);
 
         $data = $sth->fetchAll(\PDO::FETCH_ASSOC);
-        
+
         return $data;
     }
 
